1220cb9dccfdf9525d7d03e2445925376a1e51d9
niuyuxian <ncc0706@gmail.com>
2018-04-13 13:21:35 +0800

    模板方法

---
+++ 04-template/src/test/java/io/arukas/AppTest.java
@@ -0,0 +1,19 @@
+package io.arukas;
+
+import org.junit.Test;
+
+public class AppTest {
+
+    @Test
+    public void charDisplayTest() {
+        AbstractDisplay d1 = new CharDisplay('H');
+        d1.display();
+    }
+
+    @Test
+    public void strDisplayTest(){
+        AbstractDisplay d2 = new StringDisplay("Hello World.");
+        d2.display();
+    }
+
+}

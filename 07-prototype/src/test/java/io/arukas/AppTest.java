131f489d7e6c871f4e8a03aea25d436ea6cd4e02
niuyuxian <ncc0706@gmail.com>
2018-04-25 17:23:37 +0800

    原型模式

---
+++ 07-prototype/src/test/java/io/arukas/AppTest.java
@@ -0,0 +1,16 @@
+package io.arukas;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+    @Test
+    public void shouldAnswerWithTrue() {
+
+    }
+}
